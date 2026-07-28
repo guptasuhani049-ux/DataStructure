983c47268e726e32381a91010d24a5c47699c36e
guptasuhani049-ux <guptasuhani049@gmail.com>
2026-07-28 23:16:13 +0530

    Refactor stack implementation in EX2.c
    Fixed syntax errors and improved output messages.

--- EX2.c
+++ EX2.c
@@ -1,77 +1,86 @@
 #include<stdio.h>
 #define max 40
+
 int stack[max];
 int top=-1;
+
 void push(int a){
     if(top==max-1){
-        printf("stack is full");
+        printf("stack is full\n");
     }
     else{
         top++;
         stack[top]=a;
-        printf("%d is pushed value",a);
+        printf("%d is pushed value\n",a);
     }
 }
-//pop
+
 void pop(){
+    int a;
     if(top==-1){
-        printf("empty");
+        printf("empty\n");
     }
     else{
         a=stack[top];
         top--;
-        printf("%d",a);
+        printf("%d popped\n",a);
     }
 }
+
 void peek(){
     if(top==-1){
-        printf("empty");
+        printf("empty\n");
     }
     else{
-        printf("%d",stack[top]);
+        printf("%d\n",stack[top]);
     }
 }
+
 void display(){
-    if("top==-1"){
-        printf("stack is empty");
+    if(top==-1){
+        printf("stack is empty\n");
     }
     else{
-        for(int i=[top];i>=0;i--){
-    printf("%d",stack[i]);
+        for(int i=top;i>=0;i--){
+            printf("%d ",stack[i]);
         }
-    printf("\n")  
+        printf("\n");
     }
 }
+
 int main(){
-    int choice;int a;
+    int choice;
+    int a;
+
     do{
         printf("MENU\n");
         printf("1.push\n");
-        printf("2.pop\n");       
-        printf("3.peek\n");   
+        printf("2.pop\n");
+        printf("3.peek\n");
         printf("4.display\n");
-        printf("Enter your choice:"):
+        printf("Enter your choice:");
         scanf("%d",&choice);
-         
+
         switch(choice){
             case 1:
-            int a:
-            printf("enter the value to push:");
-            scanf("%d",&a);
-            push(a);
-            break;}
+                printf("enter the value to push:");
+                scanf("%d",&a);
+                push(a);
+                break;
             case 2:
-            pop();
-            break;
+                pop();
+                break;
             case 3:
-            peek();
-            break;
+                peek();
+                break;
             case 4:
-            display();
-            break:
+                display();
+                break;
+            default:
+                printf("Invalid choice\n");
+                break;
         }
-
     }while(choice!=4);
-    return 0;
 
+    return 0;
 }
